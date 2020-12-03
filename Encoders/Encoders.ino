7bdb366c1194e9f7ecb364f0f5314c4374a33be9
AngelaYu-3 <yuangela12@gmail.com>
2020-12-02 21:35:04 -0800

    use encoders to move specified distances

---
+++ Encoders/Encoders.ino
@@ -0,0 +1,59 @@
+#include <Wire.h>
+#include <Zumo32U4.h>
+
+Zumo32U4LCD lcd;
+Zumo32U4ButtonA buttonA;
+Zumo32U4Encoders encoders;
+Zumo32U4Motors motors;
+
+double TICKS_TO_DIST = 0.013614; //tiks to cm
+
+void setup() {
+  // put your setup code here, to run once:
+  lcd.clear();
+  encoders.init();
+}
+
+void driveTo(double distance, double motorSpeed){
+    double isBackward = false;
+    double currTicks = 0; 
+    double currDist = 0;
+    double targetTicks = distance / TICKS_TO_DIST;
+
+    if(motorSpeed < 0){
+      isBackward = true;
+    }
+    
+    motors.setSpeeds(motorSpeed, motorSpeed);
+    
+    while(abs(currTicks) < targetTicks){
+      currTicks += (encoders.getCountsAndResetLeft() + encoders.getCountsAndResetRight())/2;
+      currDist = currTicks * TICKS_TO_DIST;
+      lcd.clear();
+      lcd.print(currDist);
+      delay(2);
+    }
+
+    motors.setSpeeds(0, 0);
+    //lcd.clear();
+    delay(200);
+    /*lcd.print("target reached!");
+    
+    for(int i = 0; i < 16; i++){
+      lcd.scrollDisplayLeft();
+      delay(150);
+    }*/
+}
+
+void loop() {
+  // put your main code here, to run repeatedly:
+  bool isPressed = buttonA.isPressed();
+  
+  if(isPressed){   
+      driveTo(30, 80);  //30 cm forwards
+      driveTo(15, -80); //15 cm backwards
+      driveTo(7.5, 60); //7.5 cm forwards
+      driveTo(3.75, -60); //3.75 cm backwards
+      
+  }
+}
